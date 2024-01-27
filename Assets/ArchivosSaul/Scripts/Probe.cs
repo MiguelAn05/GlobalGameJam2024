4b9014ceaa8ff693a1bc1a81c462b1e4b4296793
SaulCamachoD <114762933+SaulCamachoD@users.noreply.github.com>
2024-01-27 09:05:58 -0500

    MovementPlayer

--- Assets/ArchivosSaul/Scripts/Probe.cs
+++ Assets/ArchivosSaul/Scripts/Probe.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class Probe : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Animator animator;
     void Start()
     {
-        
+        // Obtener el componente Animator del GameObject
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKey(KeyCode.D))
+        {
+            // Activar la animación de mover hacia la derecha
+            animator.SetBool("Walk", true);
+        }
+        else
+        {
+            // Desactivar la animación de mover hacia la derecha
+            animator.SetBool("Walk", false);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // Activar la animación de mover hacia la derecha
+            animator.SetTrigger("Jump");
+        }
+        
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            // Activar la animación especial asociada a la tecla F
+            animator.SetTrigger("Action1");
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            // Activar la animación especial asociada a la tecla F
+            animator.SetTrigger("Action2");
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            // Activar la animación especial asociada a la tecla F
+            animator.SetTrigger("Action3");
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            // Activar la animación especial asociada a la tecla F
+            animator.SetTrigger("Action4");
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            // Activar la animación especial asociada a la tecla F
+            animator.SetTrigger("Floor");
+        }
+       
     }
 }
