ae60a59dc98140769801197596638b0b7118b12e
DevilishhSmile <manug.diazg@gmail.com>
2024-01-26 21:34:29 -0500

    SCRIPTS
    combo y 2puntuacion

--- Assets/Scripts/Puntuacion.cs
+++ Assets/Scripts/Puntuacion.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Puntuacion : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private int score = 0;
+    public TMP_Text puntaje; // Asigna tu objeto TextMeshProUGUI desde el Inspector
 
-    // Update is called once per frame
     void Update()
     {
-        
+        CheckInput();
+        UpdateUI();
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AddScore(50);
+        }
+        else if (Input.GetKeyDown(KeyCode.J))
+        {
+            AddScore(20);
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            AddScore(60);
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            AddScore(10);
+        }
+    }
+
+    void AddScore(int points)
+    {
+        score += points;
+        Debug.Log("Puntuación actual: " + score);
+    }
+
+    void UpdateUI()
+    {
+        if (puntaje != null)
+        {
+            puntaje.text = "Puntuación: " + score;
+        }
     }
 }
+
+  
