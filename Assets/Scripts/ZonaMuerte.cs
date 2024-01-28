65c64cf4247b6aeac94a2e739c75cc13226c6b32
MiguelAn05 <miguelingil89@gmail.com>
2024-01-28 14:47:41 -0500

    Scena muerte y limites

---
+++ Assets/Scripts/ZonaMuerte.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ZonaMuerte : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("player"))
+        {
+            print("se murio");
+            PlayerPrefs.SetInt("gameOver", 1);
+
+            SceneManager.LoadScene("GameOver", 0);
+        }
+
+    }
+ 
+}
+
+
